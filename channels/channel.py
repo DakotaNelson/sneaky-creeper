85005fc94025e90ea1c77183ecf6012ca1a541f7
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-02-19 19:33:32 -0500

    added intent comments

---
+++ channels/channel.py
@@ -0,0 +1,1 @@
+# This file will contain a class that can be inherited from to create a transmission module to transfer data.
---
+++ main.py
@@ -0,0 +1,3 @@
+# This file will pull everything together:
+# Take and process command line args -> read in data -> encode data (potentially multiple times) -> transmit data
+# OR  Take and process command line args -> use transmit module's retrieve method to grab data -> write out data
