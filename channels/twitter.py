5cc5779a65cc542f9d9ea89b1b85e69fa5cf2dd1
buttegab <gabriel.butterick@students.olin.edu>
2015-02-26 21:36:39 -0500

    Twitter module and authenticator

--- channels/twitter.py
+++ channels/twitter.py
@@ -1,41 +1,81 @@
 
-
-from modules import module
-from datetime import datetime
-
-class Twitter(module.Module):
+import requests
+from urllib.parse import parse_qs
+import json
+import time
+#from modules import module
+#from datetime import datetime
+APP_KEY = input('App Key: ')
+APP_SECRET = input('App Secret: ')
+OAUTH_TOKEN = input('OAuth Token: ')
+OAUTH_TOKEN_SECRET = input('OAuth Token Secret: ') 
+  
+class Twitter():
     ''' adapted from recon-ng Twitter module
         written by Tim Tomes (@LaNMaSteR53) '''
 
     def __init__(self):
-        return
+        # APP_KEY = input('App Key: ')
+        # APP_SECRET = input('App Secret: ')
+        # OAUTH_TOKEN = input('OAuth Token: ')
+        # OAUTH_TOKEN_SECRET = input('OAuth Token Secret: ')    
+        return        
 
-    def run(self, locname, lat, lon, rad):
-        url = 'https://api.twitter.com/1.1/search/tweets.json'
-        count = 0
-        pins = []
-        self.output('Collecting data from Twitter...')
-        point = str(lat) + "," + str(lon)
-        results = self.search_twitter_api({'q':'', 'geocode': '%s,%dkm' % (point, rad), 'count': '100'})
+    def run(self, screen_name = 'EnglishmanJohny', count = 20):
+        #url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        results = self.search_twitter_api({'screen_name': screen_name, 'count': count})
 
         for tweet in results:
-            if not tweet['geo']:
-                continue
             tweet_id = tweet['id_str']
-            source = 'Twitter'
             screen_name = tweet['user']['screen_name']
-            profile_name = tweet['user']['name']
-            profile_url = 'https://twitter.com/%s' % screen_name
-            media_url = 'https://twitter.com/%s/statuses/%s' % (screen_name, tweet_id)
-            thumb_url = tweet['user']['profile_image_url_https']
             message = tweet['text']
-            latitude = tweet['geo']['coordinates'][0]
-            longitude = tweet['geo']['coordinates'][1]
-            time = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
-            pins.append(self.createPin(source, screen_name, profile_name, profile_url, media_url, thumb_url, message, latitude, longitude, time))
-            count += 1
-        self.addPins(locname, pins)
-        #self.verbose('%s tweets processed.' % (len(results)))
-        #self.summarize(new, count)
+            print (message)
+            #time = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
 
 
+
+    def search_twitter_api(self, payload):
+        headers = {'Authorization': 'Bearer %s' % (OAUTH_TOKEN)}
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        results = []
+        while True:
+            resp = self.request(url, content=payload, headers=headers)
+            jsonobj = resp.json()
+            #for item in ['error', 'errors']:
+            #    if item in jsonobj:
+            #        self.error(jsonobj[item])
+            #        raise ModuleException(jsonobj[item])
+            results += jsonobj['statuses']
+            if 'next_results' in jsonobj['search_metadata']:
+                max_id = parse_qs(jsonobj['search_metadata']['next_results'][1:])['max_id'][0]
+                payload['max_id'] = max_id
+                time.sleep(2) # don't hit the rate limit
+                # TODO: make this more intelligent about using the full limit
+                continue
+            break
+        return results
+
+
+    def request(self, url, method="GET", timeout=None, payload=None, headers=None, cookiejar=None, auth=None, content='', redirect=True):
+        if(method.lower() == "get"):
+            r = requests.get(url, params=content, headers=headers, cookies=cookiejar, auth=auth, data=payload, timeout=timeout)
+        elif(method.lower() == "post"):
+            r = requests.post(url, params=content, headers=headers, cookies=cookiejar, auth=auth, data=payload, timeout=timeout)
+        else:
+            #raise ModuleException("Only GET and POST requests are currently supported.")
+            return None
+
+        # TODO: Other things that would be nice to support:
+        #request.user_agent = self.global_options['user-agent']
+        #request.debug = self.global_options['debug']
+        #request.proxy = self.global_options['proxy']
+
+        if r.status_code == requests.codes.ok:
+            return r
+        else:
+            #raise ModuleException("Request to " + url + " returned with error " + str(r.status_code) + ".\n Response body: " + r.text)
+            return None
+if __name__ == "__main__":
+    twitter = Twitter()
+
+    twitter.run()
