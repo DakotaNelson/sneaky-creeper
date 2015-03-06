a7a94832ba73cadf128688ec075840033137add1
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-03-05 19:41:59 -0500

    refactored command line parser

--- main.py
+++ main.py
@@ -2,27 +2,31 @@
 # Take and process command line args -> read in data -> encode data (potentially multiple times) -> transmit data
 # OR  Take and process command line args -> use transmit module's retrieve method to grab data -> write out data
 
+# get list of available modules
+# TODO make this detect modules instaed of being manual
+channelOptions = ['twitter']
+encodingOptions = ['base64']
+
 # command line arguments:
 # you can input multiple transfer arguments, so -transfer and encoder will give a
 # list of arguments. Need to change use encoder and use channel to loop
 import argparse
 parser = argparse.ArgumentParser(description = 'Use social media as a tool\
-                                 for data exfiltration.', epilog = "Aw yee.")
-parser.add_argument('-transfer', '-t', dest = 'channelName', action = 'append',
-                             help = 'Choose location to transfer to \
-                             (e.g, -transfer twitter). Channels that are supported:\
-                              twitter (not really yet)', required = False) #change to True when done
-parser.add_argument('-encode', '-e', dest = 'encoderName', action = 'append',
-                             help = 'Choose methods of encoding (done in order given).\
-                              Current methods: none :-(', required = False) #change to True when done
+                                 for data exfiltration.')
+parser.add_argument('--channel', '-c', dest = 'channelName', metavar="channel_name", action = 'store',
+                             help = 'Choose a channel to transfer data over \
+                             (e.g, --transfer twitter). Channels available: '+\
+                             ", ".join(channelOptions), required = False) #change to True when done
+parser.add_argument('--encode', '-e', dest = 'encoderNames', metavar="encoder_name", nargs='+', action = 'store',
+                             help = 'Choose one or more methods of encoding (done in order given).\
+                             Encoders available: ' + ", ".join(encodingOptions), required = False) #change to True when done
+
 args = parser.parse_args()
 d =  vars(args)
-if d.get('channelName', -1) != -1: #if arguement is put in
-    channelName = d['channelName']
-if d.get('encoderName', -1) != -1:
-    encoderName = d['encoderName']
+channelName = d.get('channelName')
+encoderNames = d.get('encoderNames')
 
-print channelName, encoderName
+print channelName, encoderNames
 
 # to use an encoder:
 import importlib
