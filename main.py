fc91ced68972d4591b4dc3b93f08f79eaf48bdd1
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-03-05 19:55:01 -0500

    cleaned up some more

--- main.py
+++ main.py
@@ -4,50 +4,52 @@
 
 # get list of available modules
 # TODO make this detect modules instaed of being manual
-channelOptions = ['twitter']
-encodingOptions = ['base64']
+channelOptions = ['twitter', 'exampleChannel']
+encodingOptions = ['base64', 'exampleEncoder']
 
 # command line arguments:
 # you can input multiple transfer arguments, so -transfer and encoder will give a
 # list of arguments. Need to change use encoder and use channel to loop
 import argparse
-parser = argparse.ArgumentParser(description = 'Use social media as a tool\
-                                 for data exfiltration.')
+parser = argparse.ArgumentParser(description = 'Use social media as a tool for data exfiltration.')
 parser.add_argument('--channel', '-c', dest = 'channelName', metavar="channel_name", action = 'store',
                              help = 'Choose a channel to transfer data over \
                              (e.g, --transfer twitter). Channels available: '+\
-                             ", ".join(channelOptions), required = False) #change to True when done
+                             ", ".join(channelOptions), required = True)
 parser.add_argument('--encode', '-e', dest = 'encoderNames', metavar="encoder_name", nargs='+', action = 'store',
                              help = 'Choose one or more methods of encoding (done in order given).\
-                             Encoders available: ' + ", ".join(encodingOptions), required = False) #change to True when done
+                             Encoders available: ' + ", ".join(encodingOptions), required = True)
 
 args = parser.parse_args()
 d =  vars(args)
 channelName = d.get('channelName')
 encoderNames = d.get('encoderNames')
 
-print channelName, encoderNames
+print("")
+print("Pipeline: " + "-> ".join(encoderNames) + "-> " + channelName)
+print("")
 
-# to use an encoder:
 import importlib
-encoderName = 'exampleEncoder'
-moduleName = '.'.join(['encoders', encoderName])
-enc = importlib.import_module(moduleName)
-# This is a programmatic equivalent of:
-# from encoding import exampleEncoder as enc
 
-ret = enc.encode('this is my data')
-print(ret)
+# TODO read in data from stdin or a file
+data = 'this is my data'
+for encoderName in encoderNames:
+    moduleName = '.'.join(['encoders', encoderName])
+    enc = importlib.import_module(moduleName)
+    # This is a programmatic equivalent of:
+    # from encoding import exampleEncoder as enc
 
+    data = enc.encode(data)
+
+print(data)
 
 # to use a channel:
-channelName = 'exampleChannel'
 moduleName = '.'.join(['channels', channelName])
 chan = importlib.import_module(moduleName)
 
 # send some stuff
-chan.send("some data")
+chan.send(data)
 
 # receive some stuff
-ret = chan.receive()
-print(ret)
+resp = chan.receive()
+print(resp)
