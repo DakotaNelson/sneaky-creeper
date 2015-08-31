c6c5f91334ce8fedf4c2df2124a5bc692ab38258
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-08-30 21:56:04 -0400

    converted AES encoder to new format

--- sneakers/encoders/aes.py
+++ sneakers/encoders/aes.py
@@ -1,21 +1,27 @@
 import scrypt
 import base64
 
-requiredParams = {
-    'encode': {
-        'key': 'string used as encryption key'
-    },
-    'decode': {
-        'key': 'string used as decryption key'
+class AES():
+    requiredParams = {
+        'encode': {
+            'key': 'string used as encryption key'
+        },
+        'decode': {
+            'key': 'string used as decryption key'
+        }
     }
-}
 
+    def __init__(self):
+        pass
 
-def encode(data, params):
-    en = scrypt.encrypt(data, params['key'].encode('ascii'), 0.1)
-    return base64.b64encode(en)
+    def set_params(self, params):
+        self.params = params
 
-def decode(data, params):
-    de = base64.b64decode(data)
-    return scrypt.decrypt(de, params['key'].encode('ascii'))
+    def encode(self, data, params={}):
+        en = scrypt.encrypt(data, params['key'].encode('ascii'), 0.1)
+        return base64.b64encode(en)
+
+    def decode(self, data, params={}):
+        de = base64.b64decode(data)
+        return scrypt.decrypt(de, params['key'].encode('ascii'))
 
