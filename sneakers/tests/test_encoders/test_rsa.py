431937b15a5cc28490dc20cf4b66b31e84e4251b
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-11-04 10:47:38 -0500

    rsa test now run-location-independent, can skip if key not found, moved key files to config folder

--- sneakers/tests/test_encoders/test_rsa.py
+++ sneakers/tests/test_encoders/test_rsa.py
@@ -3,20 +3,26 @@ import string
 import random
 import os
 from Crypto.PublicKey import RSA
+from unittest.case import SkipTest
 from sneakers.encoders.rsa import Rsa
+
+import sneakers
+basePath = os.path.dirname(os.path.abspath(sneakers.__file__))
 
 class TestRsa(unittest.TestCase):
     def setUp(self):
         self.randText = ''.join([random.choice(string.letters) for i in range(10)])
 
-        self.rsa = Rsa()
-        self.rsa.params['encode']['publicKey'] = os.path.join(
-          "test_encoders", "rsa_test_keys", "test_key.pub"
-        )
+        pubPath = os.path.join(basePath, 'config', 'test_key.pub')
+        privPath = os.path.join(basePath, 'config', 'test_key')
 
-        self.rsa.params['decode']['privateKey'] = os.path.join(
-          "test_encoders", "rsa_test_keys", "test_key"
-        )
+        if not os.path.isfile(pubPath) or not os.path.isfile(privPath):
+            raise SkipTest('could not access RSA keypair in config folder')
+
+        self.rsa = Rsa()
+        self.rsa.params['encode']['publicKey'] = pubPath
+
+        self.rsa.params['decode']['privateKey'] = privPath
 
     def test_encode(self):
         encoded = self.rsa.encode(self.randText)
