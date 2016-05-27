9783f2aa3b4e92d9a78e574b245ab9be6a6aebaf
davinerd <anathema@anche.no>
2016-05-27 19:10:07 +0100

    not really a commit here

--- sneakers/encoders/b64.py
+++ sneakers/encoders/b64.py
@@ -5,9 +5,11 @@ import base64
 class B64(Encoder):
 
     def encode(self, data):
+	# we really don't need it
         params = self.reqParams['encode']
         return base64.b64encode(data)
 
     def decode(self, data):
+	# we really don't need it
         params = self.reqParams['decode']
         return base64.urlsafe_b64decode(str(data))
